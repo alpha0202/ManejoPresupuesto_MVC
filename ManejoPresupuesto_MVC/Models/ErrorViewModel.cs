f3a0ac68eb688ae9a1e35c3c915defd80adf3cb1
edwin martinez <edwmartinezg@outlook.com>
2023-08-25 22:23:13 -0500

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManejoPresupuesto_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
